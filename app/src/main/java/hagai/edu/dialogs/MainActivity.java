7c0774890c6a85a29bbe155263e5171cecac1ec8
Hagai Zamir <hagai34@gmail.com>
2017-04-25 21:38:01 +0300

    update

--- app/src/main/java/hagai/edu/dialogs/MainActivity.java
+++ app/src/main/java/hagai/edu/dialogs/MainActivity.java
@@ -2,6 +2,7 @@ package hagai.edu.dialogs;
 
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
+import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -43,6 +44,62 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
 
     @Override
     public void onClick(DialogInterface dialog, int which) {
+        switch (which){
+            case AlertDialog.BUTTON_POSITIVE:
+                Toast.makeText(this, "Quiting",Toast.LENGTH_SHORT).show();
+                System.exit(0);
+                break;
+            case AlertDialog.BUTTON_NEGATIVE:
+                Toast.makeText(this, "Staying",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+
+    public void showListDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        final String[] colors = {"Red" , "Green", "Blue"};
+
+        builder.setTitle("Pick A Color");
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setItems(colors, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String selectColor = colors[which];
+                Toast.makeText(MainActivity.this,selectColor,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        builder.show();
+    }
+
+    public void toppings(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final String [] toppings = {"Onion" , "Tomato" , "Lettuce"};
+
+        builder.setTitle("Pick A Toppings").setMessage("Do you wish to quit")
+                .setPositiveButton("Quit",this)
+                .setNegativeButton("Stay",this);
+        ;
+
+        builder.setMultiChoiceItems(R.array.toppings, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                switch (which){
+                    case AlertDialog.BUTTON_POSITIVE:
+                        Toast.makeText(this, "Stay",Toast.LENGTH_SHORT).show();
+                        System.exit(0);
+                        break;
+                    case AlertDialog.BUTTON_NEGATIVE:
+                        Toast.makeText(this, "Quit",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+
+            }
+        });
 
     }
 }
